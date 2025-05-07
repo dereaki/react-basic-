32a967262091a91edfcc0dd1420fed26749df246
Dereje <derejetesfaye834@gmail.com.>
2025-05-07 01:57:48 -0700

    add

--- src/component/week_1/EventHandler/exersise1.jsx
+++ src/component/week_1/EventHandler/exersise1.jsx
@@ -3,21 +3,43 @@ import React, { useState } from "react";
 const Exersise1 = () => {
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+
   const changeText = (e) => {
     setText(e.target.value);
+    setCount(count + 1);
   };
+
+  const resets = () => {
+    setCount(0);
+    setText("");
+  };
+  const deletes = (e) => {
+    if (e.key === "Backspace" || text.length < 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="border-blue-900 1px">
-      <h1>{text}</h1>
+    <div className="w-2/4 p-10 m-20 bg-teal-400 border border-indigo-600 rounded-lg">
+      <h1>{count}</h1>
       <div>
         <textarea
-          className="mt-4 font-mono ml-60"
+          className="font-mono rounded-lg"
           style={{
             border: "1px solid black",
+            width: "100%",
           }}
+          value={text}
           onChange={changeText}
+          onKeyDown={deletes}
         ></textarea>
       </div>
+      <button
+        onClick={resets}
+        className="p-2 font-bold text-white bg-blue-500 rounded-lg"
+      >
+        reset
+      </button>
     </div>
   );
 };
